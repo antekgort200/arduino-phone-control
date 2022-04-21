d3ef6200cba5b5a866f58d126235dcdedefd0905
antekgort200 <81635525+antekgort200@users.noreply.github.com>
2022-04-21 19:23:36 +0200

    Update and rename c.py to phone-script.py

--- phone-script.py
+++ phone-script.py
@@ -3,7 +3,7 @@ import socket
 s = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
 host = socket.gethostname()
 port = 3456
-s.connect(("192.168.43.1",port))
+s.connect(("ip of arduino/PC connected to arduino",port))
 
 msg = s.recv(1024)
 s.close()
