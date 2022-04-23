2f25f81f86c52917bc8146f900c78e713ef20685
antekgort200 <81635525+antekgort200@users.noreply.github.com>
2022-04-23 21:19:53 +0200

    Update phone-script.py

--- phone-script.py
+++ phone-script.py
@@ -6,5 +6,8 @@ port = 3456
 s.connect(("ip of arduino/PC connected to arduino",port))
 
 msg = s.recv(1024)
+
+socket.send(dytes("TEST","utf-8"))
+
 s.close()
-print(msg.decode("ascii"))
+print(msg.decode())
