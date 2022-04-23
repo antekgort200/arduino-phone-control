9c802ca6daee0f8a4a8e99364f30acf8556a8cf4
antekgort200 <81635525+antekgort200@users.noreply.github.com>
2022-04-23 21:17:52 +0200

    Update arduino-script.py

--- arduino-script.py
+++ arduino-script.py
@@ -1,24 +1,29 @@
 import socket
 import pyfirmata
 
-board = pyfirmata.Arduino("your arduino port")
+board = pyfirmata.Arduino("/dev/ttyUSB0")
 it = pyfirmata.util.Iterator(board)
 it.start()
 
 
+s = socket.socket()
+print("Sock made")
 
-serversocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-port = 8080
-
-host = socket.gethostname()
-serversocket.bind(("your ip",port))
-
-serversocket.listen(5)
-
-clientsocket,addr = serversocket.accept()
-print(f"Połączonko od: {str(addr)}")
-board.digital[7].write(1)
+port = 34567
+s.bind(('',port))
+s.listen(5)
+print("listening")
+i = 0
 while True:
-    msg = "fan start" + "\r\n"
-    clientsocket.send(msg.encode("ascii"))
+    c,addr = s.accept()
+    print("connected ",addr)
+    i +=1
+    c.send(b"TEST")
+    c.close()
+    if i % 2 == 0:
+        board.digital[7].write(1)
+        print("on")
+    else:
+        print("off")
+        board.digital[7].write(0)
 
